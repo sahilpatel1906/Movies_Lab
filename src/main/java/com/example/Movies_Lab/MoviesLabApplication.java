bd319fdc887be49e2d70dfcc4c4cbf022cf0a609
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-27 09:33:29 -0600

    first commit

---
+++ src/main/java/com/example/Movies_Lab/MoviesLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.Movies_Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Movies_Lab/MoviesLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movies_Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
